5f0b14f6cf91a07fa6c850ad8a31d391d00ce504
brucktings <fabianadsuar@gmail.com>
2025-05-05 11:43:06 +0200

    split off stats.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+from stats import num_words
+
 def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		text = f.read()
 	return text
 
-def num_words(path_to_file, encoding='utf-8'):
-	text = get_book_text(path_to_file)
-	words = text.split()
-	return len(words)
 
 def main():
-    print(f"{num_words('books/frankenstein.txt')} words found in the document")
+	full_text = get_book_text('books/frankenstein.txt')
+	print(f"{num_words(full_text)} words found in the document")
 main()
